3b497ab2b17bde5a23e3c081f286cc8461bb875f
Oray Sebastião <oraymaza@gmail.com>
2025-01-27 22:09:52 -0300

    Consumindo Api do Cep

---
+++ Integração/Interface/IViaCepIntegracao.cs
@@ -0,0 +1,9 @@
+﻿using Gestor_de_tarefas.Integração.Response;
+
+namespace Gestor_de_tarefas.Integração.Interface
+{
+    public interface IViaCepIntegracao
+    {
+        Task<ViaCepResponse> ObterDadosViaCep(string cep);
+    }
+}
---
+++ Integração/Refit/IViaCepIntegracaoRefit.cs
@@ -0,0 +1,11 @@
+﻿using Gestor_de_tarefas.Integração.Response;
+using Refit;
+
+namespace Gestor_de_tarefas.Integração.Refit
+{
+    public interface IViaCepIntegracaoRefit
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<ApiResponse<ViaCepResponse>> ObterDadosViaCep(string cep);
+    }
+}
